445c9c88e93dda5695d2af75054ef4de696f2770
shen123cy <522419092@qq.com>
2018-12-07 14:27:26 +0800

    commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,6 +1,7 @@
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("Hello World111");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
