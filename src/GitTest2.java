8ab55e6fb088fa37818cb123a8747f33447d4e1e
shen123cy <522419092@qq.com>
2018-12-07 15:06:42 +0800

    commit

--- src/GitTest2.java
+++ src/GitTest2.java
@@ -2,6 +2,22 @@
 public class GitTest2 {
 
     public static void main(String[] args) {
-        System.out.println("GitTest2");
+        String s = "ss";
+        System.out.println(s);
+        int i = t1();
+        System.out.println(i);
+        System.out.println(11111111);
+
+    }
+
+    private static int t1() {
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        return 2;
     }
 }
